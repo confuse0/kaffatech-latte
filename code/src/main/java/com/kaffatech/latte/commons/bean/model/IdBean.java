0fdc364b5e94da5420e12023b57b4e66ef10d2ea
凌臻 <confuseling@hotmail.com>
2016-12-02 14:28:15 +0800

    加入通用的时间和删除标记

--- code/src/main/java/com/kaffatech/latte/commons/bean/model/IdBean.java
+++ code/src/main/java/com/kaffatech/latte/commons/bean/model/IdBean.java
@@ -1,6 +1,10 @@
 
 package com.kaffatech.latte.commons.bean.model;
 
+import com.kaffatech.latte.commons.bean.model.type.BooleanType;
+
+import java.util.Date;
+
 /**
  * @author zhen.ling
  *
@@ -12,21 +16,55 @@ public class IdBean extends BaseBean {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * ID
+	 */
 	private Long id;
 
 	/**
-	 * @return the id
+	 * 创建时间
 	 */
+	private Date createTime;
+
+	/**
+	 * 修改时间
+	 */
+	private Date updateTime;
+
+	/**
+	 * 删除标记(0:未删除;1:已删除)
+	 */
+	private BooleanType deleteFlag;
+
 	public Long getId() {
 		return id;
 	}
 
-	/**
-	 * @param id
-	 *            the id to set
-	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public BooleanType getDeleteFlag() {
+		return deleteFlag;
+	}
+
+	public void setDeleteFlag(BooleanType deleteFlag) {
+		this.deleteFlag = deleteFlag;
+	}
 }
