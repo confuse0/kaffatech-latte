1fbceb4beca244bc18e748d230c58c3e01f6c5e7
凌臻 <confuseling@hotmail.com>
2016-12-08 14:13:58 +0800

    修改通用的ExampleBean

--- code/src/main/java/com/kaffatech/latte/commons/bean/model/ExampleBean.java
+++ code/src/main/java/com/kaffatech/latte/commons/bean/model/ExampleBean.java
@@ -13,9 +13,32 @@ public class ExampleBean extends BaseBean {
 	private static final long serialVersionUID = 1L;
 
 	/**
+	 * 是否DISTINCT
+	 */
+	private Boolean distinct;
+
+	/**
 	 * OrderBy子句
 	 */
 	private String orderByClause;
+
+	/**
+	 * 起始行号
+	 */
+	private Integer start;
+
+	/**
+	 * 单页行数
+	 */
+	private Integer rows;
+
+	public Boolean getDistinct() {
+		return distinct;
+	}
+
+	public void setDistinct(Boolean distinct) {
+		this.distinct = distinct;
+	}
 
 	public String getOrderByClause() {
 		return orderByClause;
@@ -23,5 +46,21 @@ public class ExampleBean extends BaseBean {
 
 	public void setOrderByClause(String orderByClause) {
 		this.orderByClause = orderByClause;
+	}
+
+	public Integer getStart() {
+		return start;
+	}
+
+	public void setStart(Integer start) {
+		this.start = start;
+	}
+
+	public Integer getRows() {
+		return rows;
+	}
+
+	public void setRows(Integer rows) {
+		this.rows = rows;
 	}
 }
