995aaaf5d46e1f60871b90fc1a03ba302dcba2fb
凌臻 <confuseling@hotmail.com>
2016-12-13 17:16:12 +0800

    修改Date bug

--- code/src/main/java/com/kaffatech/latte/commons/bean/converter/DateConverter.java
+++ code/src/main/java/com/kaffatech/latte/commons/bean/converter/DateConverter.java
@@ -1,7 +1,6 @@
 
 package com.kaffatech.latte.commons.bean.converter;
 
-import com.kaffatech.latte.commons.bean.model.date.Time;
 import com.kaffatech.latte.commons.toolkit.base.DateUtils;
 import org.dozer.CustomConverter;
 
@@ -22,7 +21,7 @@ public class DateConverter implements CustomConverter {
 			if (sourceFieldValue instanceof String) {
 				// 字符转日期类型（秒）
 				obj = DateUtils.parseToDate((String) sourceFieldValue);
-			} else if (sourceFieldValue instanceof Time) {
+			} else if (sourceFieldValue instanceof Date) {
 				// 日期类型转字符（秒）
 				obj = DateUtils.format((Date) sourceFieldValue);
 			}
