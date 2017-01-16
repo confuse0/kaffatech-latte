523255c23c2e0096b740fdba739c6dda9e1a5212
凌臻 <confuseling@hotmail.com>
2017-01-16 11:52:48 +0800

    升级版本 优化ftp上传下载

--- code/src/main/java/com/kaffatech/latte/commons/net/ftp/util/FtpUtils.java
+++ code/src/main/java/com/kaffatech/latte/commons/net/ftp/util/FtpUtils.java
@@ -15,8 +15,6 @@ import java.io.*;
  */
 public class FtpUtils {
 
-    private static FTPClient FTP_CLIENT = new FTPClient();
-
     private static final String DEF_ENCODING = getEncoding();
 
     private static final int DEF_PORT = 21;
@@ -48,26 +46,26 @@ public class FtpUtils {
     public static boolean uploadFile(String url, int port, String encoding, String username, String password, String remoteFolder, String localPath) {
         boolean result = false;
         InputStream input = null;
+        FTPClient client = null;
         try {
             // 打开本地文件
             input = new FileInputStream(new File(localPath));
 
             // 连接FTP服务器
-            FTP_CLIENT.setControlEncoding(encoding);
-            FTP_CLIENT.connect(url, port);
+            client = connect(url, port, encoding);
 
             // 检验是否连接成功
-            if (FTPReply.isPositiveCompletion(FTP_CLIENT.getReplyCode())) {
+            if (FTPReply.isPositiveCompletion(client.getReplyCode())) {
                 // 登录
-                FTP_CLIENT.login(username, password);
+                client.login(username, password);
 
                 // 设置文件传输类型为二进制
-                FTP_CLIENT.setFileType(FTPClient.BINARY_FILE_TYPE);
+                client.setFileType(FTPClient.BINARY_FILE_TYPE);
 
                 // 转移工作目录至指定目录下
                 String fileName = FileUtils.getFileName(localPath);
-                if (FTP_CLIENT.changeWorkingDirectory(remoteFolder)) {
-                    result = FTP_CLIENT.storeFile(fileName, input);
+                if (client.changeWorkingDirectory(remoteFolder)) {
+                    result = client.storeFile(fileName, input);
                 }
             } else {
                 Log.ERROR_LOGGER.error("上传文件连接失败");
@@ -75,18 +73,7 @@ public class FtpUtils {
         } catch (IOException e) {
             Log.ERROR_LOGGER.error("FTP文件上传异常:" + e.getMessage(), e);
         } finally {
-            try {
-                // 关闭文件流
-                FileUtils.close(input);
-                // 登出
-                FTP_CLIENT.logout();
-                // 关闭连接
-                if (FTP_CLIENT.isConnected()) {
-                    FTP_CLIENT.disconnect();
-                }
-            } catch (IOException ioe) {
-                // 忽略关闭异常
-            }
+            close(input, client);
         }
         return result;
     }
@@ -118,31 +105,31 @@ public class FtpUtils {
     public static boolean downloadFile(String url, int port, String encoding, String username, String password, String remoteFolder, String localPath) {
         boolean result = false;
         OutputStream output = null;
+        FTPClient client = null;
         try {
             // 打开本地文件
             output = new FileOutputStream(new File(localPath));
 
             // 连接FTP服务器
-            FTP_CLIENT.setControlEncoding(encoding);
-            FTP_CLIENT.connect(url, port);
+            client = connect(url, port, encoding);
 
             // 检验是否连接成功
-            if (FTPReply.isPositiveCompletion(FTP_CLIENT.getReplyCode())) {
+            if (FTPReply.isPositiveCompletion(client.getReplyCode())) {
                 // 登录
-                FTP_CLIENT.login(username, password);
+                client.login(username, password);
 
                 // 设置文件传输类型为二进制
-                FTP_CLIENT.setFileType(FTPClient.BINARY_FILE_TYPE);
+                client.setFileType(FTPClient.BINARY_FILE_TYPE);
 
                 // 转移到FTP服务器目录至指定的目录下
                 String fileName = FileUtils.getFileName(localPath);
-                if (FTP_CLIENT.changeWorkingDirectory(remoteFolder)) {
+                if (client.changeWorkingDirectory(remoteFolder)) {
                     // 获取文件列表
-                    FTPFile[] fileArray = FTP_CLIENT.listFiles();
+                    FTPFile[] fileArray = client.listFiles();
                     for (FTPFile each : fileArray) {
                         if (each.getName().equals(fileName)) {
                             // 找到下载文件进行下载
-                            FTP_CLIENT.retrieveFile(fileName, output);
+                            client.retrieveFile(fileName, output);
                             break;
                         }
                     }
@@ -154,20 +141,35 @@ public class FtpUtils {
         } catch (IOException e) {
             Log.ERROR_LOGGER.error("FTP文件下载异常:" + e.getMessage(), e);
         } finally {
-            try {
-                // 关闭文件流
-                FileUtils.close(output);
-                // 登出
-                FTP_CLIENT.logout();
-                // 关闭连接
-                if (FTP_CLIENT.isConnected()) {
-                    FTP_CLIENT.disconnect();
-                }
-            } catch (IOException ioe) {
-                // 忽略关闭异常
-            }
+            close(output, client);
         }
         return result;
+    }
+
+    private static FTPClient connect(String url, int port, String encoding) throws IOException {
+        FTPClient client = new FTPClient();
+        client.setControlEncoding(encoding);
+        client.connect(url, port);
+        return client;
+    }
+
+    private static void close(Closeable closeable, FTPClient client) {
+        try {
+            // 关闭文件流
+            FileUtils.close(closeable);
+
+            // 登出
+            if (client != null) {
+                client.logout();
+            }
+
+            // 关闭连接
+            if (client != null && client.isConnected()) {
+                client.disconnect();
+            }
+        } catch (IOException ioe) {
+            // 忽略关闭异常
+        }
     }
 
     private static String getEncoding() {
