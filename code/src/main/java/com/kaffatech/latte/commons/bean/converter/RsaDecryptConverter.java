c2dbbb230b18e23f1e03ed02fe7f30d2a502a54b
凌臻 <confuseling@hotmail.com>
2017-02-22 13:43:29 +0800

    修改KeyUtils

--- code/src/main/java/com/kaffatech/latte/commons/bean/converter/RsaDecryptConverter.java
+++ code/src/main/java/com/kaffatech/latte/commons/bean/converter/RsaDecryptConverter.java
@@ -23,5 +23,5 @@ public class RsaDecryptConverter implements CustomConverter {
 		}
 		return obj;
 	}
-
+	
 }
--- code/src/main/java/com/kaffatech/latte/security/util/KeyUtils.java
+++ code/src/main/java/com/kaffatech/latte/security/util/KeyUtils.java
@@ -1,14 +1,11 @@
 package com.kaffatech.latte.security.util;
 
-import com.kaffatech.latte.ctx.base.ApplicationContextHolder;
-import com.kaffatech.latte.security.KeyGenerator;
+import com.kaffatech.latte.ctx.base.SystemProperties;
 
 /**
  * @author lingzhen on 16/11/3.
  */
 public class KeyUtils {
-
-    private static final String DEF_NAME = "keyGenerator";
 
     public static final String SIMPLE_CRYPT_KEY_NAME = "simpleCryptKey";
 
@@ -23,35 +20,31 @@ public class KeyUtils {
     public static final String SIGN_PRIVATE_KEY_NAME = "signPrivateKey";
 
     public static String getSimpleCryptKey() {
-        return getKeyGenerator().getKey(SIMPLE_CRYPT_KEY_NAME);
+        return SystemProperties.getProperty(SIMPLE_CRYPT_KEY_NAME);
     }
 
     public static String getCryptKey() {
-        return getKeyGenerator().getKey(CRYPT_KEY_NAME);
+        return SystemProperties.getProperty(CRYPT_KEY_NAME);
     }
 
     public static String getCryptPublicKey() {
-        return getKeyGenerator().getKey(CRYPT_PUBLIC_KEY_NAME);
+        return SystemProperties.getProperty(CRYPT_PUBLIC_KEY_NAME);
     }
 
     public static String getCryptPrivateKey() {
-        return getKeyGenerator().getKey(CRYPT_PRIVATE_KEY_NAME);
+        return SystemProperties.getProperty(CRYPT_PRIVATE_KEY_NAME);
     }
 
     public static String getSignPublicKey() {
-        return getKeyGenerator().getKey(SIGN_PUBLIC_KEY_NAME);
+        return SystemProperties.getProperty(SIGN_PUBLIC_KEY_NAME);
     }
 
     public static String getSignPrivateKey() {
-        return getKeyGenerator().getKey(SIGN_PRIVATE_KEY_NAME);
+        return SystemProperties.getProperty(SIGN_PRIVATE_KEY_NAME);
     }
 
-    public static String getKey(String name) {
-        return getKeyGenerator().getKey(name);
-    }
-
-    private static KeyGenerator getKeyGenerator() {
-        return (KeyGenerator) ApplicationContextHolder.getBean(DEF_NAME);
+    public static String getKeyOfKey() {
+        return SystemProperties.getKeyOfKey();
     }
 
 }
