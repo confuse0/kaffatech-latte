e60673096df40670af52bb0d837d75d6ab5d587c
凌臻 <confuseling@hotmail.com>
2017-02-28 16:08:56 +0800

    修改sftp

--- code/src/main/java/com/kaffatech/latte/commons/net/ftp/SftpClient.java
+++ code/src/main/java/com/kaffatech/latte/commons/net/ftp/SftpClient.java
@@ -2,18 +2,23 @@ package com.kaffatech.latte.commons.net.ftp;
 
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
 import com.kaffatech.latte.commons.io.model.exception.IoRuntimeException;
 import com.kaffatech.latte.commons.io.util.FileUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Vector;
 
 /**
  * @author lingzhen on 17/2/22.
  */
 public class SftpClient {
+
+    private static String TMP_POSTFIX = "_tmp";
 
     /**
      * SSH Session
@@ -30,73 +35,89 @@ public class SftpClient {
         this.sftp = sftp;
     }
 
-    /**
-     * 上传文件
-     *
-     * @param remoteFolder 文件服务器的上传目录
-     * @param localPath    本地待上传文件
-     */
     public void upload(String remoteFolder, String localPath) {
+        FileInputStream fis = null;
         try {
-            File file = new File(localPath);
             sftp.cd(remoteFolder);
-            sftp.put(new FileInputStream(file), file.getName());
-        } catch (Exception e) {
+
+            File f = new File(localPath);
+            fis = new FileInputStream(f);
+
+            // 上传到临时文件中
+            sftp.put(fis, f.getName() + TMP_POSTFIX);
+            // 上传完毕更换文件名
+            sftp.rename(f.getName() + TMP_POSTFIX, f.getName());
+        } catch (IOException e) {
             throw new IoRuntimeException(e);
+        } catch (SftpException e) {
+            throw new IoRuntimeException(e);
+        } finally {
+            FileUtils.close(fis);
         }
     }
 
-    /**
-     * 下载文件
-     *
-     * @param remotePath 文件服务器路径
-     * @param localPath  本地下载路径
-     */
     public void download(String remotePath, String localPath) {
+        String remoteFileName = FileUtils.getFileName(remotePath);
+        File f = new File(localPath);
+        if (f.exists()) {
+            throw new IllegalStateException("file already exists!");
+        }
+
+        FileOutputStream tempDownloadFos = null;
         try {
-            File file = new File(localPath);
             sftp.cd(FileUtils.getFolder(remotePath));
-            sftp.get(FileUtils.getFileName(remotePath), new FileOutputStream(file));
-        } catch (Exception e) {
+            Vector v = sftp.ls(remoteFileName);
+            if (v.size() <= 0) {
+                // 文件不存在
+                throw new IllegalStateException("file does not exist!");
+            }
+
+            File tmpFile = new File(localPath + TMP_POSTFIX);
+            if (tmpFile.exists()) {
+                // 临时文件已经存在,需要先删除原来的文件
+                if (!tmpFile.delete()) {
+                    throw new IllegalStateException("temp file can not be deleted!");
+                }
+            }
+            if (!tmpFile.createNewFile()) {
+                throw new IllegalStateException("temp file can not be created!");
+            }
+
+            // 下载到临时文件中
+            tempDownloadFos = new FileOutputStream(tmpFile);
+            sftp.get(remoteFileName, tempDownloadFos);
+
+            // 下载完毕更换文件名
+            tmpFile.renameTo(f);
+        } catch (IOException e) {
             throw new IoRuntimeException(e);
+        } catch (SftpException e) {
+            throw new IoRuntimeException(e);
+        } finally {
+            FileUtils.close(tempDownloadFos);
         }
     }
 
-    /**
-     * 删除文件
-     *
-     * @param remotePath 要删除文件所在目录
-     */
     public void delete(String remotePath) {
         try {
             sftp.cd(FileUtils.getFolder(remotePath));
             sftp.rm(FileUtils.getFileName(remotePath));
-        } catch (Exception e) {
+        } catch (SftpException e) {
             throw new IoRuntimeException(e);
         }
     }
 
-    /**
-     * 列出目录下的文件
-     *
-     * @param remoteFolder 文件服务器的目录
-     * @return
-     */
     public List<ChannelSftp.LsEntry> listFiles(String remoteFolder) {
         try {
             return sftp.ls(remoteFolder);
-        } catch (Exception e) {
+        } catch (SftpException e) {
             throw new IoRuntimeException(e);
         }
     }
 
-    /**
-     * 关闭客户端
-     */
     public void close() {
         sftp.disconnect();
         sftp.quit();
-
         sshSession.disconnect();
     }
 
