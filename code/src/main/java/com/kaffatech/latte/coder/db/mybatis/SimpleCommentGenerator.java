b4a7ff16a457c48427d35bc8e6f30d7583e3b8ea
凌臻 <confuseling@hotmail.com>
2017-05-10 16:15:24 +0800

    修改model注释

---
+++ code/src/main/java/com/kaffatech/latte/coder/db/mybatis/SimpleCommentGenerator.java
@@ -0,0 +1,38 @@
+package com.kaffatech.latte.coder.db.mybatis;
+
+import com.kaffatech.latte.commons.toolkit.base.StringUtils;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+/**
+ * @author Created by Alan on 2017/5/10.
+ */
+public class SimpleCommentGenerator extends DefaultCommentGenerator {
+
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        topLevelClass.addJavaDocLine("/*");
+        topLevelClass.addJavaDocLine(" * 表：" + introspectedTable.getFullyQualifiedTable());
+        topLevelClass.addJavaDocLine(" */");
+    }
+
+    /**
+     * 生成字段注释
+     *
+     * @param field              字段
+     * @param introspectedTable  表
+     * @param introspectedColumn 表列
+     */
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        field.addJavaDocLine("/**");
+        if (!StringUtils.isEmpty(introspectedColumn.getRemarks())) {
+            field.addJavaDocLine(" * " + introspectedColumn.getRemarks());
+        }
+        field.addJavaDocLine(" * 表字段：" + introspectedColumn.getActualColumnName());
+        field.addJavaDocLine(" */");
+    }
+
+}
